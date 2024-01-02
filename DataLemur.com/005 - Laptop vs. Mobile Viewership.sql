80803d94221ff37aebe282ef7e18d3a427ceb467
Avish Jha <avish.j@protonmail.com>
2024-01-03 01:59:22 +0530

    data lemur - q5

---
+++ DataLemur.com/005 - Laptop vs. Mobile Viewership.sql
@@ -0,0 +1,17 @@
+-- Source:
+
+SELECT
+    SUM (
+        CASE
+            WHEN v.device_type = N'laptop' THEN 1
+            ELSE 0
+        END
+    ) AS laptop_views,
+    SUM (
+        CASE
+            WHEN v.device_type IN (N'tablet', N'phone') THEN 1
+            ELSE 0
+        END
+     ) AS mobile_views
+FROM
+    viewership v;
