6ecd907b5b084b6d451940f141b0d4a0b74798e6
Avish Jha <avish.j@protonmail.com>
2024-01-06 18:08:56 +0530

    complete lesson 1

---
+++ SQLBolt.com/Lesson 1.sql
@@ -0,0 +1,26 @@
+SELECT
+    title
+FROM
+    movies;
+
+SELECT
+    director
+FROM
+    movies;
+
+SELECT
+    title,
+    director
+FROM
+    movies;
+
+SELECT
+    title,
+    year
+FROM
+    movies;
+
+SELECT
+    *
+FROM
+    movies;
