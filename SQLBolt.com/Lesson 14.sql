b0fb3eb9d49f3bdbda65ca487c0902e7d3ca953d
Avish Jha <avish.j@protonmail.com>
2024-01-13 18:50:33 +0530

    SQLBolt - complete lesson 14

---
+++ SQLBolt.com/Lesson 14.sql
@@ -0,0 +1,18 @@
+UPDATE movies
+SET
+    director = 'John Lasseter'
+WHERE
+    title = 'A Bug''s Life';
+
+UPDATE movies
+SET
+    year = 1999
+WHERE
+    title = 'Toy Story 2';
+
+UPDATE movies
+SET
+    title = 'Toy Story 3',
+    director = 'Lee Unkrich'
+WHERE
+    title = 'Toy Story 8';
