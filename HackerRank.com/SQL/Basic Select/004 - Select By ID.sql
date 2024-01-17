b865fa1ecb34c14f36839a3a071c98a5f8574c78
Avish Jha <avish.j@protonmail.com>
2024-01-18 00:32:39 +0530

    HackerRank - SQL - Basic Select - Select By ID

---
+++ HackerRank.com/SQL/Basic Select/004 - Select By ID.sql
@@ -0,0 +1,8 @@
+-- Source: https://web.archive.org/web/20240117185051/https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+
+SELECT
+    *
+FROM
+    city c
+WHERE
+    c.id = 1661;
