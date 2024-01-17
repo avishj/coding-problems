f92670bc0323b504f601dd86c0c67a48024e48c5
Avish Jha <avish.j@protonmail.com>
2024-01-17 06:42:47 +0530

    RegexOne - complete problem 2

---
+++ RegexOne.com/Problems/Problem2.java
@@ -0,0 +1,15 @@
+// Source: 
+
+import java.util.regex.Pattern;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        String regex = "(\\d{1,3})?[\\s-]?\\(?(\\d{3})\\)?[\\s-]?\\d{3}[\\s-]?\\d{4}";
+        assert Pattern.matches(regex, "415-555-1234");
+        assert Pattern.matches(regex, "650-555-2345");
+        assert Pattern.matches(regex, "(416)555-3456");
+        assert Pattern.matches(regex, "202 555 4567");
+        assert Pattern.matches(regex, "4035555678");
+        assert Pattern.matches(regex, "1 416 555 9292");
+    }
+}
