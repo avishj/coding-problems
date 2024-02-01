42108682291481a5c061f6e69220bce4bf86b6d6
Avish Jha <avish.j@protonmail.com>
2024-02-01 20:33:41 +0530

    fix: StrataScratch.com - format q10354

--- StrataScratch.com/Coding Questions/02 - Medium/10354 - Most Profitable Companies.sql
+++ StrataScratch.com/Coding Questions/02 - Medium/10354 - Most Profitable Companies.sql
@@ -1,7 +1,7 @@
 -- Source: https://web.archive.org/web/20240206204230/https://platform.stratascratch.com/coding/10354-most-profitable-companies?code_type=5
 
-SELECT TOP 3
-    fg.[company],
+SELECT
+    TOP 3 fg.[company],
     fg.[profits]
 FROM
     forbes_global_2010_2014 fg
